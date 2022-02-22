34f8b8ad44f03b4b6bedf4554979e96c84b23332
Bart de Bont <430327@student.fontys.nl>
2022-02-22 20:20:05 +0100

    Add MUI and styled-components

---
+++ pages/about.js
@@ -0,0 +1,16 @@
+import { Typography } from "@mui/material";
+import styled from "styled-components"
+
+function about() {
+  return (
+    <Container>
+      About
+    </Container>
+  )
+}
+
+export default about
+
+const Container = styled(Typography)`
+  margin: 10px;
+`;
